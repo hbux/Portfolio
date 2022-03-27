2a958897eb7045532ce735f0ce5b5c1e3b77b679
Harry Buxton <92524519+hbux@users.noreply.github.com>
2022-03-27 22:38:05 +0100

    Add contact modal

--- StaticFiles/js/main.js
+++ StaticFiles/js/main.js
@@ -2,8 +2,21 @@ function openMobileNav() {
     document.querySelector(".mobile-nav-background").style.display = "block";
 }
 
+function toggleContactModal() {
+    if (document.querySelector(".modal").style.display === "flex") {
+        document.querySelector(".modal").style.display = "none";
+    }
+    else {
+        document.querySelector(".modal").style.display = "flex";
+        document.querySelector(".mobile-nav-background").style.display = "none";
+    }
+}
+
 window.onclick = function(event) {
     if (event.target == document.querySelector(".mobile-nav-background")) {
         document.querySelector(".mobile-nav-background").style.display = "none";
+    }
+    else if (event.target == document.querySelector(".modal")) {
+        document.querySelector(".modal").style.display = "none";
     }
   }
