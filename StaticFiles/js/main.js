5572807bfbad01197cbf8a161a829565a00fdd35
Harry Buxton <92524519+hbux@users.noreply.github.com>
2022-03-28 11:07:21 +0100

    Add resume dropdown

--- StaticFiles/js/main.js
+++ StaticFiles/js/main.js
@@ -12,11 +12,28 @@ function toggleContactModal() {
     }
 }
 
+function toggleDropdown() {
+    document.querySelector(".dropdown-content").classList.toggle("show");
+    document.querySelector(".main-cta-resume svg").classList.toggle("rotate");
+  }
+
 window.onclick = function(event) {
+    if (!event.target.matches('.main-cta-resume')) {
+        document.querySelector(".main-cta-resume svg").classList.remove("rotate");
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+    }
+
     if (event.target == document.querySelector(".mobile-nav-background")) {
         document.querySelector(".mobile-nav-background").style.display = "none";
     }
     else if (event.target == document.querySelector(".modal")) {
         document.querySelector(".modal").style.display = "none";
     }
-  }
+}
